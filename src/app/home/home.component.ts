f6f2871bb5471149d56de67059d027df39c78e93
JanakPoojary <janakrajpoojari@gmail.com>
2020-07-08 18:42:41 +0530

    home page modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogserveService } from '../logserve.service';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  note:string;
+  loggedin:boolean;
 
-  constructor() { }
+  constructor(private ls: LogserveService) { }
 
   ngOnInit(): void {
+    this.loggedin=this.ls.isloggedIn;
   }
 
 }

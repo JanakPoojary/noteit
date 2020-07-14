24f1d113b6d2aff125a5b0d684005aa84b7464bd
JanakPoojary <janakrajpoojari@gmail.com>
2020-07-14 15:06:00 +0530

    cannot use splice for undefined fixed

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value) {
-    return value.slice().reverse();
+    if(value!=undefined){
+      return value.slice().reverse();
+    }
+    
   }
 
 }
